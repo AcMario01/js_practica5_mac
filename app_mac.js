69a6024378bd2c31802e7f694d711425b59b0bd0
AcMario01 <163412332+AcMario01@users.noreply.github.com>
2025-02-22 23:39:20 +0100

    Create app_mac.js

---
+++ app_mac.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //list all card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
